6e8ac8a40054aa15c99a2d6c451e3c360f3ccc17
Marian Silviu Talmacel <mariansilviutalmacel@Marians-MacBook-Pro.local>
2020-09-24 10:03:33 +0100

    small changes

--- src/components/Subtotal/Subtotal.js
+++ src/components/Subtotal/Subtotal.js
@@ -1,9 +1,30 @@
 import React from "react";
 import CurrencyFormat from "react-currency-format";
-import { Container } from "./Subtotal.styled";
+import { Container, SubtotalGift } from "./Subtotal.styled";
 
 const Subtotal = ({ ...props }) => {
-  return <Container {...props}></Container>;
+  return (
+    <Container {...props}>
+      <CurrencyFormat
+        renderText={(value) => (
+          <>
+            <p>
+              Subtotal (0 items):
+              <strong>0</strong>
+            </p>
+            <SubtotalGift>
+              <input type="checkbox" /> This order contains a gift
+            </SubtotalGift>
+          </>
+        )}
+        decimalScale={2}
+        value={0}
+        displayType="text"
+        thousandSeparator={true}
+        prefix="£"
+      />
+    </Container>
+  );
 };
 
 export default Subtotal;
--- src/components/Subtotal/Subtotal.styled.js
+++ src/components/Subtotal/Subtotal.styled.js
@@ -7,3 +7,5 @@ export const Container = styled.div`
   width: 100%;
   height: 100%;
 `;
+
+export const SubtotalGift = styled.small``;
