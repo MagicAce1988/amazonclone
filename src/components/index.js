aee85c951814f831ef907977ef36a3eee8275230
Marian Silviu Talmacel <mariansilviutalmacel@Marians-MacBook-Pro.local>
2020-10-05 23:32:34 +0100

    animation with react flip move added plus scroll to top

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,13 @@ import { default as Product } from "./Product";
 import { default as Subtotal } from "./Subtotal";
 import { default as CheckoutProduct } from "./CheckoutProduct";
 import { default as OrderSummary } from "./OrderSummary";
+import { default as ScrollToTop } from "./ScrollToTop";
 
-export { Header, Product, Subtotal, CheckoutProduct, OrderSummary };
+export {
+  Header,
+  Product,
+  Subtotal,
+  CheckoutProduct,
+  OrderSummary,
+  ScrollToTop,
+};
